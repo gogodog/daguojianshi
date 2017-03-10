4c53149887ab3f81fd5c7639f8a063d5ee45598e
关悦 <yue.guan@corp.elong.com>
2017-03-10 20:21:45 +0800

    修改代码格式

--- src/main/java/com/dgjs/test/TestMyBatis.java
+++ src/main/java/com/dgjs/test/TestMyBatis.java
@@ -12,9 +12,6 @@ import com.dgjs.test.dao.SysRightDao;
   
 public class TestMyBatis { 
     private static Logger logger = Logger.getLogger(TestMyBatis.class);  
-    
-    
-    
     @SuppressWarnings("resource")
 	public static void main(String art[]){
         GenericXmlApplicationContext context = new GenericXmlApplicationContext();
