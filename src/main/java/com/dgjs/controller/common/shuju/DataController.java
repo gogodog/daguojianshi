3eba6ea66f8e43b37882ad2212d7aef4ca3ef829
jiadong.wen <jiadong.wen@corp.elong.com>
2017-05-27 17:34:52 +0800

    系统添加5个等级的日志：debug，console，info，error，Fatal

--- src/main/java/com/dgjs/controller/common/shuju/DataController.java
+++ src/main/java/com/dgjs/controller/common/shuju/DataController.java
@@ -12,16 +12,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.alibaba.fastjson.JSONObject;
 import com.dgjs.service.common.DataService;
 
+import freemarker.log.Logger;
+
 @Controller
 @RequestMapping("/data")
 public class DataController {
 	
 	@Autowired
 	DataService dataSerivce;
+	private Logger log = Logger.getLogger(this.getClass().getName()); 
 	
 	@RequestMapping(value="/dadian",method=RequestMethod.POST)
 	@ResponseBody
     public boolean dadian(HttpServletRequest request, @RequestBody String dadian){
+		log.setCategoryPrefix("china-大国简史");
+		log.info("info::");
+		log.error("error::");
+		log.debug("debug::");
 		return dataSerivce.dadian(request, dadian);  
     }
 	
