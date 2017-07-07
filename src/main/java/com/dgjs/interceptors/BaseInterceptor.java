f93802135c7353c279c768d5dc5291e63286d4a9
关悦 <yue.guan@corp.elong.com>
2017-07-07 13:18:40 +0800

    新增错误日志，修改联系方式

--- src/main/java/com/dgjs/interceptors/BaseInterceptor.java
+++ src/main/java/com/dgjs/interceptors/BaseInterceptor.java
@@ -6,14 +6,26 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.alibaba.fastjson.JSON;
+import com.dgjs.utils.IPUtils;
+
+import freemarker.log.Logger;
+
 public class BaseInterceptor implements HandlerInterceptor{
 
+	
+	private Logger log = Logger.getLogger(this.getClass().getName()); 
+	
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler) throws Exception {
-		// TODO Auto-generated method stub
 		request.setAttribute("contextPath", request.getContextPath());
+		if(request.getRequestURI().contains("phpMyAdmin")){
+			String ua = request.getHeader("User-Agent");
+			String ip = IPUtils.getIpAddr3(request);
+			log.error("phpMyAdmin exception,ua=["+JSON.toJSONString(ua)+"],ip=["+ip+"]");
+		}
 		return true;
 	}
 
