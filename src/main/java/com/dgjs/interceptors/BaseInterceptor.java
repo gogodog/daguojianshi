9f32b53a01f382d40c9b435f3cc7feb896c75b15
jiadong.wen <jiadong.wen@corp.elong.com>
2017-07-19 18:32:37 +0800

    判断手机端和pc端

--- src/main/java/com/dgjs/interceptors/BaseInterceptor.java
+++ src/main/java/com/dgjs/interceptors/BaseInterceptor.java
@@ -6,11 +6,22 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.dgjs.utils.MachineUtils;
+
 public class BaseInterceptor implements HandlerInterceptor{
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		request.setAttribute("contextPath", request.getContextPath());
+		String userAgent = request.getHeader("USER-AGENT");  
+        boolean isphone =MachineUtils.check(userAgent);  
+        if(isphone){  
+            //手机访问操作
+        	System.out.println("我是手机...");
+        }else{
+        	//pc访问操作
+        	System.out.println("我是电脑...");
+        }
 		return true;
 	}
 
