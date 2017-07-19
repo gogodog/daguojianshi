4b40ef05bc40788059ffd4ae758f67b565eeba60
jiadong.wen <jiadong.wen@corp.elong.com>
2017-07-19 13:21:58 +0800

    1:提交异常返回映射机制2:修改目前的异常返回默认页面

---
+++ src/main/java/com/dgjs/model/map/ErrorMapModel.java
@@ -0,0 +1,10 @@
+package com.dgjs.model.map;
+
+public class ErrorMapModel {
+	String name;
+	String type;
+	String code;
+	String tip;
+	String problem;
+	String log;
+}
---
+++ src/main/java/com/dgjs/utils/XmlUtils.java
@@ -0,0 +1,37 @@
+package com.dgjs.utils;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import com.dgjs.map.ErrorMap;
+
+public class XmlUtils {
+    /** 
+     * 将file类型的xml转换成对象 
+     */  
+    public static Object convertXmlFileToObject(@SuppressWarnings("rawtypes") Class clazz, String xmlPath) {  
+        Object xmlObject = null;  
+        try {  
+            JAXBContext context = JAXBContext.newInstance(clazz);  
+            Unmarshaller unmarshaller = context.createUnmarshaller();  
+            FileReader fr = null;  
+            try {  
+                fr = new FileReader(xmlPath);  
+            } catch (FileNotFoundException e) {  
+                e.printStackTrace();  
+            }  
+            xmlObject = unmarshaller.unmarshal(fr);  
+        } catch (JAXBException e) {  
+            e.printStackTrace();  
+        }  
+        return xmlObject;  
+    } 
+    
+    public static void main(String[] args) {
+		ErrorMap.getInstance();
+	}
+}
