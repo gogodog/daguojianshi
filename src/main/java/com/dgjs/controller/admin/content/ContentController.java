f81408243ca08d32da6b3d6d4fac1a6fc067dd4a
关悦 <yue.guan@corp.elong.com>
2017-12-15 16:45:19 +0800

    去掉浏览日志

--- src/main/java/com/dgjs/controller/admin/content/ContentController.java
+++ src/main/java/com/dgjs/controller/admin/content/ContentController.java
@@ -7,14 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.dgjs.annotation.LogRecord;
-import com.dgjs.model.enums.OperateEnum;
-
 @Controller
 public class ContentController {
 
 	@RequestMapping("/admin")
-	@LogRecord(operate=OperateEnum.Browse,remark="admin首页展示")
     public ModelAndView helloWord(HttpServletRequest request, HttpServletResponse response) throws Exception {  
 		ModelAndView mv = new ModelAndView("admin/content/index");  
         return mv;  
