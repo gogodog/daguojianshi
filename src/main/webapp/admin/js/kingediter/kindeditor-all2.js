0ed84738077f94af7b7168996545f800ca2b7044
jiadong.wen <ducycat@163.com>
2017-12-16 23:35:05 +0800

    编辑页图片插入功能，增加图片预览

--- src/main/webapp/admin/js/kingediter/kindeditor-all2.js
+++ src/main/webapp/admin/js/kingediter/kindeditor-all2.js
@@ -7184,7 +7184,7 @@ KindEditor.plugin('image', function(K) {
 			'</div>',
 			'<div class="ke-dialog-row">',
 			'<label for="remoteUrl" style="width:60px;">请选择素材</label>',
-			'<select id="remoteUrlsel" class="ke-input-text" name="url" style="width:200px;" onchange="$(\'#remoteUrl\').val(this.options[this.options.selectedIndex].value)"><option value="http://" selected>请选择</option>',
+			'<select id="remoteUrlsel" class="ke-input-text" name="url" style="width:200px;" onchange="setImage(this.options[this.options.selectedIndex])"><option value="http://" selected>请选择</option>',
 			/*'<option value="http://img0.imgtn.bdimg.com/it/u=38591822,3103715740&fm=213&gp=0.jpg">name1</option><option value="url2">name2</option>',*/
 			'optiondesss',
 			'</select>',
@@ -7227,7 +7227,7 @@ KindEditor.plugin('image', function(K) {
 		        type : "GET",
 		        dataType : "json",
 		        success : function(result) {
-		        	console.log("success::" + JSON.stringify(result));
+		        	//console.log("success::" + JSON.stringify(result));
 		            for(var r=0  ; r<result.length ; r++){
 		            	re += "<option value=\""+result[r].address+"\">"+result[r].name+"</option>";
 		            }	
@@ -9887,3 +9887,21 @@ KindEditor.plugin('fixtoolbar', function (K) {
         self.afterCreate(init);
     }
 });
+function setImage(url){
+	$('#remoteUrl').val(url.value);
+	var shownode = document.getElementById("showImg");
+	if(shownode != null){
+		shownode.parentNode.removeChild(shownode);
+	}
+	if(!url.value ||url.value == "http://"){
+		$('#remoteTitle').val("");
+		console.log(1);
+		return;
+	}
+	$('#remoteTitle').val(url.text);
+	var pdiv = document.getElementsByClassName('ke-dialog-default')[0];
+	var w = pdiv.offsetWidth;
+	var h = pdiv.offsetHeight;
+	var html = "<div id='showImg' class='ke-dialog-shadow' style=' border:1px solid #000;overflow:hidden;width:50%;max-width:50%;height:auto;max-height:50%;left:"+(w+h/4)+"px;top:"+h/4+"px'><img src='"+url.value+"' style='max-width:300px;_width:expression(this.width > 300 ? \"300px\" : this.width);width: "+w/2+"px;'></div>";
+	pdiv.insertAdjacentHTML("afterBegin",html);
+}
